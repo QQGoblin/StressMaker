5c797d5ea29df47eec2e11ce6f4ea7b2f2782475
LQing <linqing@ruijie.com.cn>
2025-04-16 14:13:43 +0800

    init

--- pkg/tools/tools_unix.go
+++ pkg/tools/tools_unix.go
@@ -13,30 +13,31 @@ const (
 	_CPUSetSize          = 1024
 )
 
-// 定义 cSet 结构体
 type _CPUSet struct {
 	bits [_CPUSetSize / 64]uint64
 }
 
-// GetTickCount64 获取系统启动后的毫秒数
 func GetTickCount64() uint64 {
 	return uint64(time.Now().UnixMilli())
 }
 
-// SetThreadAffinity 设置线程的 CPU 亲和性
 func SetThreadAffinity(idx int) error {
 
-	var cpuSet _CPUSet
+	var (
+		cpuSet _CPUSet
+		err    syscall.Errno
+	)
+
 	cpuSet.bits[idx/64] |= 1 << (uint(idx) % 64)
-	_, _, err := syscall.Syscall(
+
+	if _, _, err = syscall.Syscall(
 		_SysSchedSetAffinity,
 		uintptr(syscall.Gettid()),
 		uintptr(unsafe.Sizeof(cpuSet)),
 		uintptr(unsafe.Pointer(&cpuSet)),
-	)
-
-	if err != 0 {
+	); err != 0 {
 		return err
 	}
+
 	return nil
 }
--- pkg/tools/tools_windows.go
+++ pkg/tools/tools_windows.go
@@ -14,23 +14,21 @@ var (
 	procGetTickCount64        = kernel32.NewProc("GetTickCount64")
 )
 
-// GetTickCount64 获取系统启动后的毫秒数
 func GetTickCount64() uint64 {
 	ret, _, _ := procGetTickCount64.Call()
 	return uint64(ret)
 }
 
-// SetThreadAffinity 设置线程的 CPU 亲和性
 func SetThreadAffinity(idx int) error {
 
-	threadHandle := windows.CurrentThread()
-
-	var mask uint64 = 1 << (idx - 1)
-	r1, _, err := procSetThreadAffinityMask.Call(
-		uintptr(threadHandle),
-		uintptr(mask),
+	var (
+		threadHandle        = windows.CurrentThread()
+		mask         uint64 = 1 << (idx - 1)
+		err          error
+		ret          uintptr
 	)
-	if r1 == 0 {
+
+	if ret, _, err = procSetThreadAffinityMask.Call(uintptr(threadHandle), uintptr(mask)); ret == 0 {
 		return fmt.Errorf("SetThreadAffinityMask failed: %v", err)
 	}
 
